9a4dec9a9b9c2d597e6c6f1564c4b2447683d79d
r3t4k3r <g1fl3x228@gmail.com>
2024-12-18 03:36:54 -0500

    edit install.sh

--- run.sh
+++ run.sh
@@ -8,14 +8,9 @@ echo "delete existing connections"
 nmcli connection delete "wifi"
 nmcli connection delete "hotspot"
 
-echo "change mac"
+echo "change mac, maybe its didn't work, i'm not sure"
 macchanger -r $NETWORK_DEVICE
 echo "start hotspot"
 nmcli device wifi hotspot ifname $HOTSPOT_DEVICE ssid $HOTSPOT_SSID password $HOTSPOST_PASS
 echo "connect to network"
-nmcli connection add type wifi ifname $NETWORK_DEVICE con-name "wifi" ssid $NETWORK_SSID
-nmcli connection modify "wifi" wifi-sec.key-mgmt wpa-psk
-nmcli connection modify "wifi" wifi-sec.psk $NETWORK_PASS
-nmcli connection up "wifi"
-# nmcli dev wifi connect $NETWORK_SSID password $NETWORK_PASS ifname $NETWORK_DEVICE
-
+nmcli dev wifi connect $NETWORK_SSID password $NETWORK_PASS ifname $NETWORK_DEVICE
